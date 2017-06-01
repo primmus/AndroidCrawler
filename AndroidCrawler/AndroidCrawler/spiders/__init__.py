6cd8983124e988a14d6fcc0e231638f99def2bf5
letheascetic <yizhifight@163.com>
2017-06-01 09:56:47 +0800

    resolve runspiders.py spiders/__init__.py

--- AndroidCrawler/AndroidCrawler/spiders/__init__.py
+++ AndroidCrawler/AndroidCrawler/spiders/__init__.py
@@ -3,8 +3,6 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 
-market_name = 'Market_Hiapk'
-table_name = 'Market_Hiapk'
-submitter = 'Market_Hiapk'
+
 
 
